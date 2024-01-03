cf275eec2662ac9673a0443e4916555ebdb3c71c
Isaac Campos <info@icampos.me>
2024-01-03 01:22:13 -0600

    Update versions

--- src/browser/face/client-storage.js
+++ src/browser/face/client-storage.js
@@ -1,3 +1,3 @@
-import ClientMuseria from 'museria/dist/client/museria.client.js';
+import ClientMuseria from 'museria/dist/client/metasound.client.js';
 const client = new ClientMuseria({ address: [], task: { workerChangeInterval: '1m' } });
 export default client;
